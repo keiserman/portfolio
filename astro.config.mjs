dded0e194f63aecdbf9e6dd6ceaab6ff3b0d0146
Kevin <eisermanka@gmail.com>
2024-07-02 18:25:54 -0400

    Test build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://keiserman.github.io",
-  base: "/portfolio",
+  base: "portfolio",
 });
