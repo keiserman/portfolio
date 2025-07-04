bea244210ffc5285535abae6f0fed0c126ec05fa
Kevin Eiserman <eisermanka@gmail.com>
2025-07-03 21:03:30 -0400

    Add pricing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import alpinejs from "@astrojs/alpinejs";
+
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), alpinejs()],
 });
